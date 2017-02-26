05b21e48cd94251a47f3641b746e13377784ec38
Sergiy Loza <sergiy.loza@lemberg.co.uk>
2017-02-26 11:01:36 +0200

    Add project structure

---
+++ ShadowEdgeExample/LIbrary/ShadowEdge/ShadowEdge/ShadowEdge.h
@@ -0,0 +1,19 @@
+//
+//  ShadowEdge.h
+//  ShadowEdge
+//
+//  Created by Sergiy Loza on 26.02.17.
+//  Copyright © 2017 Sergiy Loza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ShadowEdge.
+FOUNDATION_EXPORT double ShadowEdgeVersionNumber;
+
+//! Project version string for ShadowEdge.
+FOUNDATION_EXPORT const unsigned char ShadowEdgeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShadowEdge/PublicHeader.h>
+
+
---
+++ ShadowEdgeExample/ShadowEdgeExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShadowEdgeExample
+//
+//  Created by Sergiy Loza on 26.02.17.
+//  Copyright © 2017 Sergiy Loza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
